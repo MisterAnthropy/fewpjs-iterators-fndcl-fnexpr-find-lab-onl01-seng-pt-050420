96004dde9804a4af97a1a06f0e728a408a40ab74
MisterAnthropy <tjcroley@outlook.com>
2020-11-23 18:11:59 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let winner = record.find(w => {
+    return w.result === "W";
+  });
+  if (winner) {
+    return winner.year;
+  }
+  else {
+    return undefined
+  }
+};
